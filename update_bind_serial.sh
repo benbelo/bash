363e056ce3590a26de7bc1cad152ee1bf1b3a370
Benjamin Belot <benjamin.belot@proton.me>
2022-11-30 11:01:17 +0100

    add if condition and removed $date

--- update_bind_serial.sh
+++ update_bind_serial.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
 
-date=$(date +%Y%m%d%H)
-file=/pathto/file.db
+# copie le serial du bas a la place de celui du haut
+# sed le serial du bas avec le timestamp actuel (YYYYMMDDHH)
+
+date +%Y%m%d%H > date.txt
+
+file=db.esiee
 bottomserial=$(cat $file | grep Serial | awk '{print $1}')
 topserial=$(cat $file | grep last | awk '{print $6}')
+timestamp=$(cat date.txt)
 
-cp $file backup/
+if [[ $bottomserial = $date ]]; then
+  echo "Serials are the same, please add 1 to the bottom serial"
 
-echo "Copying actual serial into old one"
-sed -i "s/$topserial/$bottomserial/g" "$file"
+else
 
-echo "Updating new serial"
-sed "10s/$bottomserial/$date/" file.db
+  cp $file backup/.
 
-echo "Serials were udpated for $file"
+  echo "Copying actual serial into old one"
+  sed -i "s/$topserial/$bottomserial/g" "$file"
+
+  echo "Updating new serial"
+  sed -i "10s/$bottomserial/$timestamp/" "$file"
+
+  echo "Serials were udpated for $file"
+
+fi
